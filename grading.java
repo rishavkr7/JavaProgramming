7752a1d0de05d32d8caba5f88bbe0d963d59a3b3
rishavkr7 <rishav0797@gmail.com>
2022-11-22 17:52:55 +0530

    intial commit

--- grading.java
+++ grading.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class grading {
 public static void main (String[] args){
       Scanner scn = new Scanner(System.in);
-      System.out.print("Enter the Marks: ");`
+      System.out.print("Enter the Marks: ");
       int marks = scn.nextInt();
       
       if (marks>90)
